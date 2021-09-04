f7c7eb7819fcac67a86f80769c7f790d30f5dca7
Michael <mklink@yandex.ru>
2021-09-04 15:56:12 +0300

    Version optimized

---
+++ 01krsnol/krestnol_optimized.py
@@ -0,0 +1,54 @@
+def draw_board(itog):
+    for i in range(3):
+        a = board_m[0 + i * 3] if board_m[0 + i * 3] in "+o" or not itog else " "
+        b = board_m[1 + i * 3] if board_m[1 + i * 3] in "+o" or not itog else " "
+        c = board_m[2 + i * 3] if board_m[2 + i * 3] in "+o" or not itog else " "
+        print("", a, b, c, "", sep="|")
+
+def take_input(player_token):
+    while True:
+        player_answer = input("Куда поставим '" + player_token+"' ? ")
+        if player_answer not in "123456789" or len(player_answer) != 1:
+            print("Некорректный ввод. Введите число от 1 до 9.")
+            continue
+        if board_m[int(player_answer)-1] in "+o":
+            print(f"Клетка № {player_answer} занята!")
+            continue
+        return int(player_answer)
+        break
+
+
+def check_win():
+    win_coord = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
+    for each in win_coord:
+        if board_m[each[0]] == board_m[each[1]] == board_m[each[2]]:
+            print(board_m[each[0]], "выиграл! ИГРА ОКОНЧЕНА. Итоговое поле:")
+            return True
+    return False
+
+board_m = '123456789'
+counter = 0
+who_win = 0
+print("ИГРА НАЧАЛАСЬ!")
+print("Используйте цифры от 1 до 9 для установки крестика или нолика в соотвествующую клетку.")
+while True:
+    draw_board(False)
+    if counter % 2 == 0:
+        player_token="+"
+        answer = take_input(player_token)
+    else:
+        player_token="o"
+        answer = take_input(player_token)
+    print(answer)
+    board_m = board_m[:answer - 1] + player_token + board_m[answer:]
+    counter += 1
+    if counter < 4:
+        continue
+    if check_win():
+        draw_board(True)
+        break
+    if counter == 9:
+        print("Ничья! ИГРА ОКОНЧЕНА. Итоговое поле:")
+        draw_board(True)
+        break
+
